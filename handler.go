cfddadf915d506f6b099491f44121978c4134558
savaki <matt.ho@gmail.com>
2019-03-28 09:07:01 -0700

    removing the content type header

--- handler.go
+++ handler.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Matt Ho
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -88,7 +88,7 @@ func (h *Handler) reply(ctx context.Context, req *Request, input *replyInput) er
 	if err != nil {
 		return err
 	}
-	httpReq.Header.Set("Content-Type", "application/json")
+	httpReq.Header.Del("Content-Type")
 	httpReq = httpReq.WithContext(ctx)
 
 	httpResp, err := h.transport.RoundTrip(httpReq)
